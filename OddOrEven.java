8788924d96dff8209b38101a5e678536f824a80c
balaaarush <109011081+balaaarush@users.noreply.github.com>
2022-07-10 11:53:21 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=20;
+		
+		if (i%2 ==0)
+		{
+			System.out.println("The given number is Even");
+		}		
+		else
+		{
+			System.out.println("The given number is Odd");
+		}
+	}
+
+}
